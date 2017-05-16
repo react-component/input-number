373e1e11e7993351b0d3405977c249e2d1eff50e
paranoidjk <hust2012jiangkai@gmail.com>
2017-05-16 16:23:23 +0800

    chore: add use-touch demo

---
+++ examples/simple-use-touch.js
@@ -0,0 +1,49 @@
+/* eslint no-console:0 */
+import 'rc-input-number/assets/index.less';
+import InputNumber from 'rc-input-number';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Component extends React.Component {
+  state = {
+    disabled: false,
+    readOnly: false,
+    value: 5,
+  };
+  onChange = (value) => {
+    console.log('onChange:', value);
+    this.setState({ value });
+  }
+  toggleDisabled = () => {
+    this.setState({
+      disabled: !this.state.disabled,
+    });
+  }
+  toggleReadOnly = () => {
+    this.setState({
+      readOnly: !this.state.readOnly,
+    });
+  }
+  render() {
+    return (
+      <div style={{ margin: 10 }}>
+        <InputNumber
+          min={-8}
+          max={10}
+          value={this.state.value}
+          style={{ width: 100 }}
+          readOnly={this.state.readOnly}
+          onChange={this.onChange}
+          disabled={this.state.disabled}
+          useTouch
+        />
+        <p>
+          <button onClick={this.toggleDisabled}>toggle Disabled</button>
+          <button onClick={this.toggleReadOnly}>toggle readOnly</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Component/>, document.getElementById('__react-content'));
