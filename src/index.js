99ed8b44a08932ac7d4b7c46913971b34c6c4de7
Bogdan Gradinariu <bogdan.gradinariu@gmail.com>
2020-02-11 14:16:43 +0200

    Pass the `title` prop down to the actual input (#200)

--- src/index.js
+++ src/index.js
@@ -812,6 +812,7 @@ export default class InputNumber extends React.Component {
             min={props.min}
             step={props.step}
             name={props.name}
+            title={props.title}
             id={props.id}
             onChange={this.onChange}
             ref={this.saveInput}
