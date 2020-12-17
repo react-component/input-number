1388d6aa4293d133f9ae860100309bb2a8bfd2e8
rainkolwa <rainkolwa@gmail.com>
2020-12-17 21:06:33 +0800

    fix: caret position (#281)

--- tests/index.test.js
+++ tests/index.test.js
@@ -1563,6 +1563,63 @@ describe('InputNumber', () => {
     });
   });
 
+  // https://github.com/ant-design/ant-design/issues/28366
+  describe('cursor position when last string exists', () => {
+    const setUpCursorTest = (initValue, prependValue) => {
+      class Demo extends React.Component {
+        state = {
+          value: initValue,
+        };
+
+        onChange = value => {
+          this.setState({ value });
+        };
+
+        render() {
+          return (
+            <InputNumber
+              ref="inputNum"
+              value={this.state.value}
+              onChange={this.onChange}
+              formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              parser={value => value.replace(/\$\s?|(,*)/g, '')}
+            />
+          );
+        }
+      }
+      example = ReactDOM.render(<Demo />, container);
+      inputNumber = example.refs.inputNum;
+      inputNumber.input.selectionStart = 0;
+      inputNumber.input.selectionEnd = 0;
+      inputElement = ReactDOM.findDOMNode(inputNumber.input);
+      Simulate.focus(inputElement);
+      for (let i = 0; i < prependValue.length; i += 1) {
+        Simulate.keyDown(inputElement, { keyCode: keyCode.ONE });
+      }
+      Simulate.change(inputElement, { target: { value: prependValue + initValue } });
+    };
+    it('shold fix caret position on case 1', () => {
+      // '$ 1'
+      setUpCursorTest('', '1');
+      expect(inputNumber.input.selectionStart).to.be(3);
+    });
+    it('shold fix caret position on case 2', () => {
+      // '$ 111'
+      setUpCursorTest('', '111');
+      expect(inputNumber.input.selectionStart).to.be(5);
+    });
+    it('shold fix caret position on case 3', () => {
+      // '$ 111'
+      setUpCursorTest('1', '11');
+      expect(inputNumber.input.selectionStart).to.be(4);
+    });
+    it('shold fix caret position on case 4', () => {
+      // '$ 123,456'
+      setUpCursorTest('456', '123');
+      expect(inputNumber.input.selectionStart).to.be(6);
+    });
+  });
+
   describe(`required prop`, () => {
     it(`should add required attr to the input tag when get passed as true`, () => {
       ReactDOM.render(<InputNumber id="required-input-test" required />, container);
