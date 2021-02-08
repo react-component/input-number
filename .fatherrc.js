3fad1c1b692902dabbd311610e82958e81572bf6
zombiej <smith3816@gmail.com>
2021-02-08 11:21:24 +0800

    rm father pre-commit

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,7 +1,8 @@
 export default {
-  cjs: "babel",
-  esm: { type: "babel", importLibToEs: true },
+  cjs: 'babel',
+  esm: { type: 'babel', importLibToEs: true },
   preCommit: {
+    // Father preCommit is OOD. comment this tmp.
     // eslint: true,
     // prettier: true,
   },
