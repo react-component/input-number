2bfb2c1a81fecf1dbf72eaeb19138de75c8591a1
zombiej <smith3816@gmail.com>
2021-02-09 17:56:44 +0800

    back of test

---
+++ tests/keyboard.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import KeyCode from 'rc-util/lib/KeyCode';
+import { mount } from 'enzyme';
+import InputNumber from '../src';
+
+describe('InputNumber.Keyboard', () => {
+  it('up', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(<InputNumber onChange={onChange} />);
+    wrapper.find('input').simulate('keyDown', { which: KeyCode.UP });
+    expect(onChange).toHaveBeenCalledWith(1);
+  });
+
+  it('down', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(<InputNumber onChange={onChange} />);
+    wrapper.find('input').simulate('keyDown', { which: KeyCode.DOWN });
+    expect(onChange).toHaveBeenCalledWith(-1);
+  });
+});
