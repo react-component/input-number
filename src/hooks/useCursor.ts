1675a04b016c37ee5cb18288ea9687b497814f02
zombiej <smith3816@gmail.com>
2021-02-09 15:41:26 +0800

    update comment

--- src/hooks/useCursor.ts
+++ src/hooks/useCursor.ts
@@ -1,1 +1,5 @@
+/**
+ * Keep input cursor in the correct position if possible.
+ * Is this necessary since we have `formatter` which may mass the content?
+ */
 export default function useCursor() {}
