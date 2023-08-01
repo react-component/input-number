f806318e0b7744df250202044da2d89c67805eb1
二货机器人 <smith3816@gmail.com>
2023-08-01 19:48:51 +0800

    test: add test case

--- tests/input.test.tsx
+++ tests/input.test.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { render, fireEvent } from './util/wrapper';
 import KeyCode from 'rc-util/lib/KeyCode';
+import React from 'react';
 import InputNumber, { InputNumberProps } from '../src';
+import { fireEvent, render } from './util/wrapper';
 
-describe('InputNumber.Input',  () => {
-   function prepareWrapper(text: string, props?: Partial<InputNumberProps>, skipInputCheck = false) {
+describe('InputNumber.Input', () => {
+  function loopInput(input: HTMLElement, text: string) {
+    for (let i = 0; i < text.length; i += 1) {
+      const inputTxt = text.slice(0, i + 1);
+      fireEvent.change(input, { target: { value: inputTxt } });
+    }
+  }
+
+  function prepareWrapper(text: string, props?: Partial<InputNumberProps>, skipInputCheck = false) {
     const { container } = render(<InputNumber {...props} />);
-    const input = container.querySelector('input')
-    fireEvent.focus(input)
+    const input = container.querySelector('input');
+    fireEvent.focus(input);
     for (let i = 0; i < text.length; i += 1) {
       const inputTxt = text.slice(0, i + 1);
       fireEvent.change(input, { target: { value: inputTxt } });
@@ -16,8 +23,8 @@ describe('InputNumber.Input',  () => {
     if (!skipInputCheck) {
       expect(input.value).toEqual(text);
     }
-     fireEvent.blur(input)
-    return input
+    fireEvent.blur(input);
+    return input;
   }
 
   it('input valid number', () => {
@@ -59,28 +66,28 @@ describe('InputNumber.Input',  () => {
   });
 
   it('negative min with higher precision', () => {
-    const wrapper = prepareWrapper('-4', {min: -3.5, precision: 0});
+    const wrapper = prepareWrapper('-4', { min: -3.5, precision: 0 });
     expect(wrapper.value).toEqual('-3');
   });
 
   it('positive min with higher precision', () => {
-    const wrapper = prepareWrapper('4', {min: 3.5, precision: 0});
+    const wrapper = prepareWrapper('4', { min: 3.5, precision: 0 });
     expect(wrapper.value).toEqual('4');
   });
 
   it('negative max with higher precision', () => {
-    const wrapper = prepareWrapper('-4', {max: -3.5, precision: 0});
+    const wrapper = prepareWrapper('-4', { max: -3.5, precision: 0 });
     expect(wrapper.value).toEqual('-4');
   });
 
   it('positive max with higher precision', () => {
-    const wrapper = prepareWrapper('4', {max: 3.5, precision: 0});
+    const wrapper = prepareWrapper('4', { max: 3.5, precision: 0 });
     expect(wrapper.value).toEqual('3');
   });
 
   // https://github.com/ant-design/ant-design/issues/9439
   it('input negative zero', async () => {
-    const wrapper = await  prepareWrapper('-0', {}, true);
+    const wrapper = await prepareWrapper('-0', {}, true);
     expect(wrapper.value).toEqual('0');
   });
 
@@ -108,8 +115,12 @@ describe('InputNumber.Input',  () => {
 
   it('pressEnter works', () => {
     const onPressEnter = jest.fn();
-    const { container } = render(<InputNumber onPressEnter={onPressEnter} defaultValue={'5'}/>);
-    fireEvent.keyDown(container.querySelector('.rc-input-number'), { key: "Enter", keyCode: KeyCode.ENTER ,which:KeyCode.ENTER})
+    const { container } = render(<InputNumber onPressEnter={onPressEnter} defaultValue={'5'} />);
+    fireEvent.keyDown(container.querySelector('.rc-input-number'), {
+      key: 'Enter',
+      keyCode: KeyCode.ENTER,
+      which: KeyCode.ENTER,
+    });
     expect(onPressEnter).toHaveBeenCalled();
     expect(onPressEnter).toHaveBeenCalledTimes(1);
   });
@@ -130,13 +141,13 @@ describe('InputNumber.Input',  () => {
     };
 
     const { container } = render(<Demo />);
-    const input = container.querySelector('input')
-    fireEvent.focus(input)
+    const input = container.querySelector('input');
+    fireEvent.focus(input);
     fireEvent.change(input, { target: { value: '3' } });
-    fireEvent.keyDown(input,{ which: KeyCode.ENTER })
+    fireEvent.keyDown(input, { which: KeyCode.ENTER });
     expect(input.value).toEqual('3');
     fireEvent.change(input, { target: { value: '5' } });
-    fireEvent.keyDown(input,{ which: KeyCode.ENTER })
+    fireEvent.keyDown(input, { which: KeyCode.ENTER });
     expect(input.value).toEqual('5');
   });
 
@@ -146,39 +157,58 @@ describe('InputNumber.Input',  () => {
     const Demo = () => {
       const [value, setValue] = React.useState<string | number>(1);
       outSetValue = setValue;
-      return <InputNumber autoFocus value={value} onChange={val => setValue(val)} />;
+      return <InputNumber autoFocus value={value} onChange={(val) => setValue(val)} />;
     };
 
     const { container } = render(<Demo />);
-    const input = container.querySelector('input')
-    fireEvent.keyDown(input,{ which: KeyCode.TAB })
-    fireEvent.blur(input)
+    const input = container.querySelector('input');
+    fireEvent.keyDown(input, { which: KeyCode.TAB });
+    fireEvent.blur(input);
     expect(input.value).toEqual('1');
     outSetValue(5);
-    fireEvent.focus(input)
+    fireEvent.focus(input);
     expect(input.value).toEqual('5');
+  });
+
+  // https://github.com/ant-design/ant-design/issues/40733
+  it('input combo should be correct', () => {
+    const onChange = jest.fn();
+    const input = prepareWrapper('', {
+      onChange,
+      precision: 0,
+    });
+
+    onChange.mockReset();
+
+    fireEvent.focus(input);
+    loopInput(input, '1.55.55');
+    expect(onChange).not.toHaveBeenCalledWith(2);
+
+    fireEvent.blur(input);
+    expect(input.value).toEqual('2');
+    expect(onChange).toHaveBeenCalledWith(2);
   });
 
   describe('empty on blur should trigger null', () => {
     it('basic', () => {
       const onChange = jest.fn();
       const { container } = render(<InputNumber defaultValue="1" onChange={onChange} />);
-      const input = container.querySelector('input')
+      const input = container.querySelector('input');
       fireEvent.change(input, { target: { value: '' } });
       expect(onChange).toHaveBeenCalledWith(null);
 
-      fireEvent.blur(input)
+      fireEvent.blur(input);
       expect(onChange).toHaveBeenLastCalledWith(null);
     });
 
     it('min range', () => {
       const onChange = jest.fn();
       const { container } = render(<InputNumber min="1" defaultValue="11" onChange={onChange} />);
-      const input = container.querySelector('input')
+      const input = container.querySelector('input');
       fireEvent.change(input, { target: { value: '' } });
       expect(onChange).toHaveBeenCalled();
 
-      fireEvent.blur(input)
+      fireEvent.blur(input);
       expect(onChange).toHaveBeenLastCalledWith(null);
     });
   });
